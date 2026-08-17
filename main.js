c6dff5c8ee9241dcb6adf1e955ff2c74b00b2de0
gabriel39333 <gabriel.leite.rodrigues@escola.pr.gov.br>
2026-08-17 10:07:12 -0300

    oo

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 function openTab(evt, tabName) {
-    // Esconde todos os conteúdos das abas
     let tabContents = document.getElementsByClassName("tab-content");
     for (let i = 0; i < tabContents.length; i++) {
         tabContents[i].classList.remove("active");
     }
-
-    // Remove a classe "active" de todos os botões
     let tabLinks = document.getElementsByClassName("tab-btn");
     for (let i = 0; i < tabLinks.length; i++) {
         tabLinks[i].classList.remove("active");
     }
-
-    // Mostra a aba atual e adiciona a classe "active" ao botão clicado
     document.getElementById(tabName).classList.add("active");
     evt.currentTarget.classList.add("active");
+}
+function checkInput() {
+    const input = document.getElementById('taskInput');
+    const btn = document.getElementById('decomporBtn');
+    if (input.value.trim() !== '') {
+        btn.disabled = false;
+        btn.classList.remove('disabled');
+    } else {
+        
+        btn.disabled = true;
+        btn.classList.add('disabled');
+    }
+}
+function fillInput(text) {
+    const input = document.getElementById('taskInput');
+    input.value = text;
+    checkInput(); 
+    
+    input.focus();
 }
